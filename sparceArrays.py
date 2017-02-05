22e3c0a4916b55cd4b1a34d2adb097a2fbe99016
ForFer <100317714@alumnos.uc3m.es>
2017-02-05 16:40:33 +0100

    Algorithmic crush added

--- sparceArrays.py
+++ sparceArrays.py
@@ -1,7 +1,45 @@
+"""
+Author: Fernando Collado
+Github: ForFer
+"""
+
+
+"""
+There are N strings. Each string's length is no more than 20 characters.
+There are Q also queries. For each query, you are given a string, and you need to find out
+how many times this string occurred previously.
+
+Input Format
+
+The first line contains N, the number of strings.
+The next lines N each contain a string.
+The N+2nd line contains Q, the number of queries.
+The following Q lines each contain a query string.
+
+Sample Input
+
+4
+aba
+baba
+aba
+xzxb
+3
+aba
+xzxb
+ab
+
+Sample Output
+
+2
+1
+0
+
+"""
+
 s=input()
 i = int(s)
 s = []
-while(int(i)>0):
+while(i>0):
     s.append(input())
     i=i-1
 
@@ -18,3 +56,14 @@ for i in range(len(s1)):
         if(s1[i]==s[j]):
             res[i]+=1
     print(res[i])
+
+"""
+Better solution, by user georgeluisrocha
+
+n = int(input())
+strs = [input() for x in range(n)]
+q = int(input())
+for _ in range(q):
+    print(strs.count(input()))
+
+"""
