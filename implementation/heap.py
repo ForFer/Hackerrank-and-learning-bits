d5f073aa665546c9bb121ba69e4619e734f5755d
ForFer <fcollado@protonmail.com>
2017-11-12 10:46:12 +0100

    heap implementation fixed

--- implementation/heap.py
+++ implementation/heap.py
@@ -4,6 +4,9 @@ Github: ForFer
 """
 
 #min Heap implementation
+#TODO
+# - add delete_item
+# - fix get_max
 
 from math import floor
 
@@ -16,8 +19,11 @@ class Heap:
 
     #Min heap, hence 
     def insert(self, data):
+        print("insert:",data)
+        print("heap before", self.data)
         self.data.append(data)
-        self.reheapify( index=int(floor(len(self.data)/2)), option=1 )
+        self.heapifyUp()
+        print("Heap after", self.data)
 
     def get_min(self):
         return self.data[0]
@@ -32,7 +38,7 @@ class Heap:
         self.data.pop()
 
         #Start reorganizing tree from the root
-        self.reheapify( index=int(floor(len(self.data)/2)), option=0 )
+        self.heapifyDown()
 
         return root
 
@@ -42,52 +48,47 @@ class Heap:
 #                self.data = self.data[:i] + self.data[i+1:]
 #        self.reheapify(index=int(floor(len(self.data)/2)), option=1)
 
-
-    def reheapify(self, index, option):
-        """
-        reheapify down (option=0), or up(option=1) 
-        """
-        stop = False
-
-        if option==0:
-            n = 0
-            stop_n = index
-        else:
-            n = index
-            stop_n = 0
-        while not stop: 
-            parent = self.data[n]
-            left_child = self.data[2*n+1] if len(self.data)-1>=2*n+1 else None
-            right_child = self.data[2*n+2] if len(self.data)-1>=2*n+2 else None
-
-            toSwap = None            
-
-            if left_child is not None and right_child is not None:
-                if parent > min(left_child, right_child):
-                    if left_child < right_child:
-                        toSwap = 2*n+1 #left child
-                    else:
-                        toSwap = 2*n+2 #right child
-            else:
-                if left_child is not None and right_child is None:
-                    if parent > left_child:
-                        toSwap = 2*n+1 #left child
-                elif right_child is None and left_child is not None:
-                    if parent > right_child:
-                        toSwap = 2*n+2 #right_child
-
-            if toSwap:
-                self.swap(n, toSwap)
-
-            if option==0:
-                n += 1
-                if n > stop_n:
-                    stop = True
-            else:
-                n -= 1
-                if n < stop_n:
-                    stop = True
+    def heapifyUp(self):
+        index = len(self.data)-1
+        parent = floor((index-1)/2)
     
+        while index > 0:
+            if self.data[parent] > self.data[index]:
+                self.swap(parent, index)
+                index = parent
+                parent = floor((index-1)/2)
+            else:   
+                index = -1
+
+    def heapifyDown(self):
+        index = 0
+        stop = False
+    
+        while not stop:
+            current = self.data[index]
+            left = self.data[index*2+1] if len(self.data)-1 >= 2*index+1 else None
+            right = self.data[index*2+2] if len(self.data)-1 >= 2*index+2 else None
+
+            if left and right:
+                if current > min(left, right):
+                    if left < current:
+                        self.swap(index*2+1, index)
+                    elif right < current:
+                        self.swap(index*2+2, index)
+                else:
+                    stop = True
+            else:
+                if left and not right:
+                    if left < current:
+                        self.swap(index*2+1, index)
+                elif not left and right:
+                    if right < current:
+                        self.swap(index*2+2, index)
+                else:
+                    stop = True
+
+            index = index*2+1
+   
     #Ol'switcheroo, pythonic style
     def swap(self, new, old):
         self.data[old], self.data[new] = self.data[new], self.data[old]
@@ -98,6 +99,9 @@ class Heap:
 def test():
     h = Heap()
     
+    h.insert(-1)
+    print(h.data)
+
     h.insert(1)
     print(h.data)
 
@@ -108,6 +112,10 @@ def test():
 
     h.insert(0)
     print(h.data)
+
+    h.insert(-15)
+    print(h.data)
+    
 
     h.insert(50)
     print(h.data)
@@ -120,9 +128,9 @@ def test():
     print(h.data)
     print("getting root ", h.delete_root())
     print("heap after deletion ", h.data)
-    h.delete_item(50)
-    print("Removing element 50")
-    print("Heap after deletion", h.data)
+#    h.delete_item(50)
+#    print("Removing element 50")
+#    print("Heap after deletion", h.data)
         
 if __name__=="__main__":
     test()
