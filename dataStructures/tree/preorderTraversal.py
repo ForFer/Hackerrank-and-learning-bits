ca5c92b6f30bac43282a33567249f2663b19f4a3
ForFer <fcollado@protonmail.com>
2017-12-26 11:16:58 +0100

    preorder traversal of tree

---
+++ dataStructures/tree/preorderTraversal.py
@@ -0,0 +1,26 @@
+"""
+Author: Fernando Collado
+Github: ForFer
+Hackerrank: F0rz4
+"""
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def preOrder(root):
+    data = preOrderTraverse(root)
+    print(data)
+
+def preOrderTraverse(root):
+    if root:
+        node = str(root.data)
+        if root.left:
+            node = node + ' ' + preOrderTraverse(root.left)
+        if root.right:
+            node = node + ' ' + preOrderTraverse(root.right)
+        return node
+    else:
+        return ''
